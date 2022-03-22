6de0be241e279d57dbf04c9ef13a008e7a4b033d
isRoutine <gianluca.farinaccio@gmail.com>
2022-03-22 09:14:23 +0100

    update jpa-es2

---
+++ siw-jpa-es2/src/it/uniroma3/siw/model/Customer.java
@@ -0,0 +1,82 @@
+package it.uniroma3.siw.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String phonenumber;
+	private LocalDate dateOfBirth;
+	private LocalDate registrationDate;
+	
+	@OneToOne
+	private Address address;
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public LocalDate getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(LocalDate registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	
+
+	
+
+	
+	
+}
