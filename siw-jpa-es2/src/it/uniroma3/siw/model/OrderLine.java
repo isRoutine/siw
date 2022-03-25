546ad3d4474b630bd969e71120e861679b47d613
isRoutine <gianluca.farinaccio@gmail.com>
2022-03-25 11:29:49 +0100

    update jpa-es2
    slide 122

--- siw-jpa-es2/src/it/uniroma3/siw/model/OrderLine.java
+++ siw-jpa-es2/src/it/uniroma3/siw/model/OrderLine.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class OrderLine {
@@ -14,12 +15,11 @@ public class OrderLine {
 	
 	private Double unitPrice;
 	private Integer quantity;
-	public Double getUnitPrice() {
-		return unitPrice;
-	}
-	public void setUnitPrice(Double unitPrice) {
-		this.unitPrice = unitPrice;
-	}
+	
+	@ManyToOne
+	//@OneToOne (funziona uguale?) lo schema non varia
+	private Product product;
+
 	public Integer getQuantity() {
 		return quantity;
 	}
@@ -27,4 +27,11 @@ public class OrderLine {
 		this.quantity = quantity;
 	}
 		
+	public Double getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(Double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	
 }
---
+++ siw-jpa-es2/src/it/uniroma3/siw/model/Product.java
@@ -0,0 +1,44 @@
+package it.uniroma3.siw.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+
+	private String description;
+	
+	private float price;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+}
